28d453e1b69b556c71c89b74953101450f188a62
hanad-mohamud <hmohamud@torontomu.ca>
2025-01-19 17:24:18 -0500

    Updates on data
    Made progress on data collection from weather API and for power data.
    
    Next we need to add the following.
    - Calculate Wind Chill using formula
    - Fill in missing data
       -Use line method down and bfill back up
    - Add one hot encoding to calendar variables
       - Base off code developed by Jo

--- scripts/realDataGetter.py
+++ scripts/realDataGetter.py
@@ -14,48 +14,97 @@ import nest_asyncio # Allows for asyncio to be nested
 ### Functions ###
 
 # get_weather_data - For a given day and FSA (through the lat and long), get the weather data for that day
-async def get_weather_data(session: aiohttp.ClientSession, current_date: datetime, next_date: datetime, fsa, lat: float, lon: float):
+async def get_weather_data(session: aiohttp.ClientSession, current_date: datetime, next_date: datetime, lat: float, lon: float):
 
     # Set Up
     weather_data = []   # List to store weather data 
-    bbox = f'{lon-0.1},{lat-0.1},{lon+0.1},{lat+0.1}' # Bounding box for data collection
-    url = f'https://api.weather.gc.ca/collections/climate-hourly/items?bbox={bbox}&datetime={start_date.isoformat()}/{next_date.isoformat()}' # URL to get data from API
+    bbox_limit = 0.1   # Bounding box limit for data collection
 
-    # Make request to API
-    response = await session.get(url) # Make function wait for response
-    response_data = await response.json() # Get data from response
+    while True: # Loop to get data until enough data is collected (need at least 24 data points for day)
+        bbox = f'{lon-bbox_limit},{lat-bbox_limit},{lon+bbox_limit},{lat+bbox_limit}' # Bounding box for data collection
+        url = f'https://api.weather.gc.ca/collections/climate-hourly/items?bbox={bbox}&datetime={current_date.isoformat()}/{next_date.isoformat()}' # URL to get data from API
 
-    # If data was found, extract data and stron in weather_data list
-    if 'features' in response_data and len(response_data['features']) > 23:
-        for data_point in response_data['features']:
-            weather_data.append({
-                'Station Name': data_point['properties'].get('STATION_NAME'),
-                'Date/Time (LST)': data_point['properties'].get('LOCAL_DATE'),
-                'Year': data_point['properties'].get('LOCAL_YEAR'),
-                'Month': data_point['properties'].get('LOCAL_MONTH'),
-                'Day': data_point['properties'].get('LOCAL_DAY'),
-                'Hour': data_point['properties'].get('LOCAL_HOUR'),
-                'Temperature': data_point['properties'].get('TEMP'),
-                'Dew Point Temperature': data_point['properties'].get('DEW_POINT_TEMP'),
-                'Wind_Speed': data_point['properties'].get('WIND_SPEED'),
-                'Precipitation': data_point['properties'].get('PRECIP_AMOUNT'),
-                'Station Pressure': data_point['properties'].get('STATION_PRESSURE'),
-                'Humidity': data_point['properties'].get('RELATIVE_HUMIDITY'),
-                'Windchill': data_point['properties'].get('WINDCHILL'),
-            })
+        # Make request to API
+        response = await session.get(url) # Make function wait for response
+        response_data = await response.json() # Get data from response
+
+        # If enough data was found, extract data and stron in weather_data list
+        if 'features' in response_data and len(response_data['features']) > 23:
+            for data_point in response_data['features']:
+                weather_data.append({
+                    #'Station Name': data_point['properties'].get('STATION_NAME'),
+                    'Date/Time (LST)': data_point['properties'].get('LOCAL_DATE'),
+                    'Year': data_point['properties'].get('LOCAL_YEAR'),
+                    'Month': data_point['properties'].get('LOCAL_MONTH'),
+                    'Day': data_point['properties'].get('LOCAL_DAY'),
+                    'Hour': data_point['properties'].get('LOCAL_HOUR'),
+                    'Temperature': data_point['properties'].get('TEMP'),
+                    'Dew Point Temperature': data_point['properties'].get('DEW_POINT_TEMP'),
+                    'Wind_Speed': data_point['properties'].get('WIND_SPEED'),
+                    'Station Pressure': data_point['properties'].get('STATION_PRESSURE'),
+                    'Humidity': data_point['properties'].get('RELATIVE_HUMIDITY'),
+                    'Windchill': data_point['properties'].get('WINDCHILL'),
+                })
+            
+            break # Break loop if enough data is found
+
+        else: # If not enough data is found, increase the bounding box to get more data
+            bbox_limit += 0.05
+            await asyncio.sleep(0.5) # Add a slight delay to avoid overloading the server
 
     # Turn list of dictionaries into a pandas dataframe
     weather_data_df = pd.DataFrame(weather_data)
 
-    return weather_data_df   # Test to see if this will work
+    # Average out data points for the same hour - Usefull for when multiple data points are collected for the same hour from different stations
+    weather_data_df = weather_data_df.groupby(['Date/Time (LST)', 'Year', 'Month', 'Day', 'Hour']).mean(numeric_only=True).reset_index()
+
+    return weather_data_df  # Return weather data for the day
+
+# get_power_data - Get power data for a given date range and FSA by picking up data from saved CSVs
+def get_power_data(data_path, start_date: datetime, end_date: datetime, fsa):
+    
+    # Set up
+    target_dir = data_path + "/Hourly_Demand_Data"
+    filtered_monthly_power_data = [pd.DataFrame()]
+    power_data = pd.DataFrame()
+    monthly_power_data = []
+
+    # Get data from IESO
+    for year in range(start_date.year, end_date.year + 1):
+        for month in ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']: # Loop through all months
+            file_path = f'{target_dir}/PUB_HourlyConsumptionByFSA_{year}{month}_v1.csv'
+            if os.path.exists(file_path): # Check if file exists
+
+                # Use try and catch if problems reading input data
+                try: 
+                    power_data = pd.read_csv(file_path, skiprows=3, header = 0, usecols= ['FSA', 'DATE', 'HOUR', 'CUSTOMER_TYPE', 'TOTAL_CONSUMPTION'])
+                except FileNotFoundError: # not all months had a file (for example, 2024 only has up to may)
+                    continue
+                except ValueError: # skiprows=x does not match the "normal sequence" of 3. For example, 2023 08 data had a different skip_row value
+                    power_data = pd.read_csv(file_path, skiprows=7, header = 0, usecols= ['FSA', 'DATE', 'HOUR', 'CUSTOMER_TYPE', 'TOTAL_CONSUMPTION'])
+
+                # Filter power data for residential power and for terget FSA
+                monthly_power_data.append(power_data[(power_data['FSA'] == fsa) & (power_data['CUSTOMER_TYPE'] == 'Residential')])
+                
+    # Concatenate all monthly power data into one dataframe
+    monthly_power_data_df = pd.concat(monthly_power_data)
+    filtered_power_data_df = monthly_power_data_df.groupby(["FSA", "CUSTOMER_TYPE", "HOUR", "DATE"]).sum().reset_index()
+
+    # Modify Calander Columns
+    filtered_power_data_df['DATE'] = pd.to_datetime(filtered_power_data_df['DATE'])
+    filtered_power_data_df['YEAR'] = filtered_power_data_df['DATE'].dt.year
+    filtered_power_data_df['MONTH'] = filtered_power_data_df['DATE'].dt.month
+    filtered_power_data_df['DAY'] = filtered_power_data_df['DATE'].dt.day
+    
+    return filtered_power_data_df
 
 # get_data_for_time_range - get data for a given time range for a given latitude and longitude
-async def get_data_for_time_range(start_date: datetime, end_date: datetime, fsa, lat: float, lon: float):
+async def get_data_for_time_range(data_path, start_date: datetime, end_date: datetime, fsa, lat: float, lon: float):
 
     # Weather Data Collection - Done asynchronously wtih Weather API #
     async with aiohttp.ClientSession() as session: # Using session to make requests
         
-            # List to store all the jobs to be run asynchronously
+        # List to store all the jobs to be run asynchronously
         weather_jobs = []
         
         # Get current date
@@ -67,7 +116,7 @@ async def get_data_for_time_range(start_date: datetime, end_date: datetime, fsa,
             next_date = current_date + timedelta(days=1) - timedelta(hours=1) 
 
             # Get weather data for current day
-            weather_jobs.append(get_weather_data(session, current_date, next_date, fsa, lat, lon))
+            weather_jobs.append(get_weather_data(session, current_date, next_date, lat, lon))
 
             # Move to next day
             current_date = current_date + timedelta(days=1)  
@@ -79,7 +128,10 @@ async def get_data_for_time_range(start_date: datetime, end_date: datetime, fsa,
         weather_data_list = [data for data in retuned_data if not data.empty] # Get all data that is not empty
         weather_data = pd.concat(weather_data_list) if weather_data_list else pd.DataFrame() # Concatenate all dataframes into one dataframe
 
-    return weather_data
+    # Collect Power Data - Pick up data from saved CSV from IESO
+    power_data = get_power_data(data_path, start_date, end_date, fsa)
+
+    return weather_data, power_data
 
 
 
@@ -137,7 +189,7 @@ start_day = 1
 start_hour = 0
 
 end_year = 2018
-end_month = 1
+end_month = 12
 end_day = 31
 end_hour = 23
 
@@ -146,7 +198,8 @@ start_date = datetime(start_year, start_month, start_day, start_hour,0,0)
 end_date = datetime(end_year, end_month, end_day, end_hour,0,0)
 
 # Collect data - Using asynchronous functions
-weather_data = asyncio.run(get_data_for_time_range(start_date, end_date, fsa, lat, lon))
+weather_data, power_data = asyncio.run(get_data_for_time_range(data_path, start_date, end_date, fsa, lat, lon))
 
 # Save data to CSV
-weather_data.to_csv(f'{target_dir}/weather_data_{fsa}_{start_date.strftime("%Y%m%d")}_{end_date.strftime("%Y%m%d")}.csv', index=False)
+weather_data.to_csv(f'{target_dir}/weather_data_{fsa}_{start_date.strftime("%Y%m%d")}_{end_date.strftime("%Y%m%d")}_V4.csv', index=False)
+power_data.to_csv(f'{target_dir}/power_data_{fsa}_{start_date.strftime("%Y%m%d")}_{end_date.strftime("%Y%m%d")}_V5.csv', index=False)
